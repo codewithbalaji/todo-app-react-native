44b4eecf9eeaf4a824caca56e9eb1d5a2aefb4c9
balaji <114200498+codewithbalaji@users.noreply.github.com>
2024-08-04 13:51:05 +0530

    final

--- App.js
+++ App.js
@@ -1,11 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [editIndex, setEditIndex] = useState(-1);
+
+  const handleAddTask = () => {
+    if (task) {
+      if (editIndex !== -1) {
+        // Edit exiting task
+        const updatedTasks = [...tasks];
+        updatedTasks[editIndex] = task;
+        setTasks(updatedTasks);
+        setEditIndex(-1);
+      } else {
+        // Add new task
+        setTasks([...tasks, task]);
+      }
+      setTask("");
+    }
+  };
+
+  const renderItem = ({ item, index }) => {
+    return (
+      <View style={styles.task}>
+        <Text style={styles.taskText}>{item}</Text>
+        <View style={styles.taskActions}>
+          <TouchableOpacity onPress={() => handleEditTask(index)}>
+            <Text style={styles.editBtn}>Edit</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.deleteBtn} onPress={()=>handleDeleteTask(index)}>Delete</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
+  const handleEditTask = (index) => {
+    const taskToEdit = tasks[index];
+    setTask(taskToEdit);
+    setEditIndex(index);
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index,1);
+    setTasks(updatedTasks);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>CodeWithBalaji</Text>
+      <Text style={styles.title}>ToDo App</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Task"
+        value={task}
+        onChangeText={(text) => setTask(text)}
+      />
+      <TouchableOpacity style={styles.addButton} onPress={handleAddTask}>
+        <Text style={styles.addButtonText}>
+          {" "}
+          {editIndex !== -1 ? "Update Task" : "Add Task"}
+        </Text>
+      </TouchableOpacity>
+      <FlatList
+        data={tasks}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 }
@@ -13,8 +86,60 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 40,
+    marginTop: 40,
+  },
+  heading: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 7,
+    color: "dodgerblue",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 3,
+    borderColor: "#ccc",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 10,
+    fontSize: 18,
+  },
+  addButton: {
+    backgroundColor: "dodgerblue",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  addButtonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+  },
+  task: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 15,
+  },
+  taskActions: {
+    flexDirection: "row",
+  },
+  editBtn: {
+    color: "dodgerblue",
+    fontWeight: "bold",
+    fontSize: 18,
+    marginRight: 10,
+  },
+  deleteBtn: {
+    color: "red",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  taskText: {
+    fontSize: 19,
   },
 });
